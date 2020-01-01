11e58249b21e043562764a1e4d61b380604e2a38
Anatolie Lupacescu <anatollupacescu@gmail.com>
2020-01-01 15:15:50 +0100

    domain object in state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import './App.css';
-import TodoObject from './domain/todo';
 import TodoForm from './component/TodoForm';
 
-class App extends React.Component {
-  
-  render() {
-    const items = [
-      { text: 'Learn JavaScript', done: false },
-      { text: 'Learn React', done: false },
-      { text: 'Play around in JSFiddle', done: true },
-      { text: 'Build something awesome', done: true },
-    ];
+export default function App() {
 
-    const todoObject = new TodoObject(items);
+  let items = [
+    { text: 'Learn JavaScript', done: false },
+    { text: 'Learn React', done: false },
+    { text: 'Play around in JSFiddle', done: true },
+    { text: 'Build something awesome', done: true },
+  ];
 
-    return (
-      <TodoForm todoObject={todoObject} />
-    );
-  }
+  return (
+    <TodoForm items={items} />
+  );
 }
-
-export default App;
--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
 import TodoItem from './TodoItem';
+import TodoObject from '../domain/todo';
 
 export default function TodoForm(props) {
   
-  let { todoObject } = props;
-  let [inputValue, setInput] = useState("");
+  let [todoObject] = useState(new TodoObject(props.items))
+
+  let [todoText, setInput] = useState("");
 
   let handleChange = (e) => {
     e.preventDefault();
@@ -15,7 +17,7 @@ export default function TodoForm(props) {
   let submitForm = (e) => {
     e.preventDefault();
     if (e.keyCode === 13) { /* Enter key pressed */
-      let added = todoObject.addItem(inputValue);
+      let added = todoObject.addItem(todoText);
       if (!added) {
         alert('we got a duplicate!')
         return
@@ -26,10 +28,11 @@ export default function TodoForm(props) {
   
   let [count, setCount] = useState(0) // to allow updates in child component to re-render parent
 
-  return (<div>
+  return (
+  <div>
     <h3>New item</h3>
     <div>
-      <input type="text" onKeyUp={submitForm} onChange={handleChange} value={inputValue} />
+      <input type="text" onKeyUp={submitForm} onChange={handleChange} value={todoText} />
     </div>
     <div>
       <h2>Todos:</h2>
